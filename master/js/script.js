9a83ecbe3e769f9987ea53e4ae641059bf0b8e7c
cavidan666666 <cavidan666666@gmail.com>
2017-10-12 02:28:46 +0400

    Add files via upload

---
+++ master/js/script.js
@@ -0,0 +1,33 @@
+// document.onmousemove = animateCircles;
+
+// 	var rengler = ['red' , 'blue' , 'green', 'greenyellow','yellow','pink'];
+
+// 	function animateCircles (event)
+// 	{
+
+// 		var circle = document.createElement("div");
+// 		circle.setAttribute("class", "circle");
+// 		document.body.appendChild(circle);
+
+// 		circle.style.left = event.clientX + 'px';
+// 		circle.style.top = event.clientY + 'px';
+  
+
+//          var reng = rengler[Math.floor(Math.random() * rengler.length)];
+//          circle.style.backgroundColor = reng;
+//          circle.style.borderColor = reng;
+
+
+// 		circle.style.transition = "all 0.5s linear 0s";
+
+// 		circle.style.left = circle.offsetLeft -20 + 'px';
+
+// 		circle.style.top = circle.Top -20 + 'px';
+
+// 		circle.style.width = "50px";
+// 		circle.style.height = "50px";
+// 		circle.style.borderWidth = "5px";
+// 		circle.style.opacity = 0;
+		
+
+// 	}
